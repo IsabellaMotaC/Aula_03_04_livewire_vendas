15ece3a6ead03690d4fae73ce5961fb5261f192b
IsabellaMotaC <isabellamotacarvalho@hotmail.com>
2025-04-03 13:58:16 -0300

    first commit

---
+++ app/Livewire/Admin/Funcionarios/FuncionarioEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Funcionarios;
+
+use Livewire\Component;
+
+class FuncionarioEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.funcionarios.funcionario-edit');
+    }
+}
---
+++ app/Livewire/Admin/Funcionarios/FuncionarioIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Funcionarios;
+
+use Livewire\Component;
+
+class FuncionarioIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.funcionarios.funcionario-index');
+    }
+}
